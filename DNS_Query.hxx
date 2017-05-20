89d9b69d6c8e2d853f47fc752fa7c91a9608bb40
lichobaron <licho_baron_leon@hotmail.com>
2017-05-20 10:44:58 -0500

    respose hxx

--- DNS_Query.hxx
+++ DNS_Query.hxx
@@ -1,12 +1,12 @@
 #ifndef __DNS_Query_HXX
 #define __DNS_Query_HXX
 
-void DNS_Query::decode(const char* buffer, int size) throw() {
-    decode_hdr(buffer);
+void DNS_Query::decodeQuery(const char* buffer, int size) throw() {
+    this->decode_hdr(buffer);
     buffer += HDR_OFFSET;
-    decode_qname(buffer);
-    m_qType = this->get16bits(buffer);
-    m_qClass = this->get16bits(buffer);
+    this->decode_qname(buffer);
+    this->m_qType = this->get16bits(buffer);
+    this->m_qClass = this->get16bits(buffer);
 }
 
 void DNS_Query::decode_qname(const char*& buffer) throw() {
@@ -46,6 +46,86 @@ void DNS_Query::decode_hdr(const char* buffer) throw () {
     this->m_anCount = get16bits(buffer);
     this->m_nsCount = get16bits(buffer);
     this->m_arCount = get16bits(buffer);
+}
+
+int DNS_Query::codeRespose(char* buffer) throw() {
+
+    char* bufferBegin = buffer;
+
+    this->code_hdr(buffer);
+    buffer += HDR_OFFSET;
+
+    // Code Question section
+    this->code_domain(buffer, m_name);
+    this->put16bits(buffer, m_type);
+    this->put16bits(buffer, m_class);
+
+    // Code Answer section
+    this->code_domain(buffer, m_name);
+    this->put16bits(buffer, m_type);
+    this->put16bits(buffer, m_class);
+    this->put32bits(buffer, m_ttl);
+    this->put16bits(buffer, m_rdLength);
+    this->code_domain(buffer, m_rdata);
+
+    int size = buffer - bufferBegin;
+
+    return size;
+}
+
+void DNS_Query::code_hdr(char* buffer) throw () {
+
+    this->put16bits(buffer, m_id);
+
+    int fields = (m_qr << 15);
+    fields += (m_opcode << 14);
+    //...
+    fields += m_rcode;
+    this->put16bits(buffer, fields);
+
+    this->put16bits(buffer, m_qdCount);
+    this->put16bits(buffer, m_anCount);
+    this->put16bits(buffer, m_nsCount);
+    this->put16bits(buffer, m_arCount);
+}
+
+void DNS_Query::put16bits(char*& buffer, unsigned int value) throw () {
+
+    buffer[0] = (value & 0xFF00) >> 8;
+    buffer[1] = value & 0xFF;
+    buffer += 2;
+}
+
+void DNS_Query::code_domain(char*& buffer, const std::string& domain) throw() {
+
+    int start(0), end; // indexes
+
+    while ((end = domain.find('.', start)) != string::npos) {
+
+        *buffer++ = end - start; // label length octet
+        for (int i=start; i<end; i++) {
+
+            *buffer++ = domain[i]; // label octets
+        }
+        start = end + 1; // Skip '.'
+    }
+
+    *buffer++ = domain.size() - start; // last label length octet
+    for (int i=start; i<domain.size(); i++) {
+
+        *buffer++ = domain[i]; // last label octets
+    }
+
+    *buffer++ = 0;
+}
+
+void DNS_Query::put32bits(char*& buffer, ulong value) throw () {
+
+    buffer[0] = (value & 0xFF000000) >> 24;
+    buffer[1] = (value & 0xFF0000) >> 16;
+    buffer[2] = (value & 0xFF00) >> 16;
+    buffer[3] = (value & 0xFF) >> 16;
+    buffer += 4;
 }
 
 #endif
