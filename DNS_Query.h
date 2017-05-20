5d53e775b47f6fc5b6bec8168d2124631db46e05
lichobaron <licho_baron_leon@hotmail.com>
2017-05-20 10:28:41 -0500

    query

--- DNS_Query.h
+++ DNS_Query.h
@@ -4,25 +4,48 @@
 #include <string>
 
 namespace DNS{
-  class Query{
+
+class Query{
+
   public:
-    int get16bits(const char*& buffer) throw();
-    void put16bits(char*& buffer, uint value) throw ();
-    void put32bits(char*& buffer, ulong value) throw ();
-    std::string asStringRespose() const throw();
+    void decodeQuery(const char* buffer, int size) throw();
+    void decode_hdr(const char* buffer) throw ();
     std::string asStringQuery() const throw();
+    int codeRespose(char* buffer) throw();
+    void decodeResponse(const char* buffer, int size) throw();
+    std::string asStringRespose() const throw();
+    int get16bits(const char*& buffer) throw();
+    void put16bits(char*& buffer, unsigned int value) throw ();
+    void put32bits(char*& buffer, unsigned long value) throw ();
+
   private:
-    std::string query_name ;
-    unsigned int q_type ;
-    unsigned int q_class ;
-    std::string m_rdata;
-    static const uint QR_MASK = 0x8000;
-    static const uint OPCODE_MASK = 0x7800;
-    static const uint AA_MASK = 0x0400;
-    static const uint TC_MASK = 0x0200;
-    static const uint RD_MASK = 0x0100;
-    static const uint RA_MASK = 0x8000;
-    static const uint RCODE_MASK = 0x000F;
+    static const unsigned int HDR_OFFSET = 12;
+    std::string m_qName;
+    unsigned int m_qType;
+    unsigned int m_qClass;
+    void decode_qname(const char*& buffer) throw();
+
+    unsigned int m_id;
+    unsigned int m_qr;
+    unsigned int m_opcode;
+    unsigned int m_aa;
+    unsigned int m_tc;
+    unsigned int m_rd;
+    unsigned int m_ra;
+    unsigned int m_rcode;
+
+    unsigned int m_qdCount;
+    unsigned int m_anCount;
+    unsigned int m_nsCount;
+    unsigned int m_arCount;
+
+    static const unsigned int QR_MASK = 0x8000;
+    static const unsigned int OPCODE_MASK = 0x7800;
+    static const unsigned int AA_MASK = 0x0400;
+    static const unsigned int TC_MASK = 0x0200;
+    static const unsigned int RD_MASK = 0x0100;
+    static const unsigned int RA_MASK = 0x8000;
+    static const unsigned int RCODE_MASK = 0x000F;
   };
 }
 #include "DNS_Server.hxx"
