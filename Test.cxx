2c12b7c3419ffbdb5bccd71fb6fd8b4a1df1529a
Santiago chaustre perdomo <santiago.chaustreperdomo@gmail.com>
2017-05-22 15:51:43 -0500

    pk

--- Test.cxx
+++ Test.cxx
@@ -2,11 +2,17 @@
 #include <exception>
 
 int main(int argc, char const *argv[]) {
-  DNS::Server s;
-  std::string file_name = "masterfile.txt" ;
-  try {
 
-    s.init(53 , file_name);
+  if(argc != 3){
+    std::cerr << "Usage: [Port] [M_Filename] \n" ;
+    return 1 ;
+  }
+  unsigned int port = atoi(argv[1]);
+  std::string file_name  = argv[2];
+
+  DNS::Server s;
+  try {
+    s.init(port , file_name);
     s.run();
   } catch (DNS::DNSException  & ex) {
     std::cout << ex.what();
