42c906d131ee50ddeb6fc7fd7433c78372c7932a
lichobaron <licho_baron_leon@hotmail.com>
2017-05-22 07:33:14 -0500

    foreign resolver

--- main.cpp
+++ main.cpp
@@ -1,6 +1,42 @@
-#include "DNS_Resolver.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
-int main ()
+#ifndef   NI_MAXHOST
+#define   NI_MAXHOST 1025
+#endif
+
+int main()
 {
-  return 0;
+    struct addrinfo* result;
+    struct addrinfo* res;
+    int error;
+
+    /* resolve the domain name into a list of addresses */
+    error = getaddrinfo("amon.cable.net.co"/*"200.118.2.88"*/, NULL, NULL, &result);
+    if (error != 0) {
+        if (error == EAI_SYSTEM) {
+            perror("getaddrinfo");
+        } else {
+            fprintf(stderr, "error in getaddrinfo: %s\n", gai_strerror(error));
+        }
+        exit(EXIT_FAILURE);
+    }
+
+    /* loop over all returned results and do inverse lookup */
+    for (res = result; res != NULL; res = res->ai_next) {
+        char hostname[NI_MAXHOST];
+        error = getnameinfo(res->ai_addr, res->ai_addrlen, hostname, NI_MAXHOST, NULL, 0, 0);
+        if (error != 0) {
+            fprintf(stderr, "error in getnameinfo: %s\n", gai_strerror(error));
+            continue;
+        }
+        if (*hostname != '\0')
+            printf("hostname: %s\n", hostname);
+    }
+
+    freeaddrinfo(result);
+    return 0;
 }
